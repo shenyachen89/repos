a2a7c56848fc1626d267ed67dcc30aeae29dec31
shenyachen <shenyachen@fuljoy.com>
2019-06-13 16:10:53 +0800

    链表的遍历查询和修改

--- data_structures/src/com/knife/linkedlist/LinkedList.java
+++ data_structures/src/com/knife/linkedlist/LinkedList.java
@@ -30,7 +30,7 @@ public class LinkedList<E> {
     private int size;
 
     public LinkedList() {
-        dummyHead = new Node(null,null);
+        dummyHead = new Node(null, null);
         size = 0;
     }
 
@@ -79,14 +79,99 @@ public class LinkedList<E> {
      * @param e
      */
     public void addFirst(E e) {
-        add(0,e);
+        add(0, e);
     }
 
     /**
      * 在链表尾添加新的元素e
+     *
      * @param e
      */
     public void addLast(E e) {
-        add(size,e);
+        add(size, e);
+    }
+
+    /**
+     * 获得链表的第index位置的元素
+     *
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed,Illeagal index");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    /**
+     * 获取链表第一个元素
+     *
+     * @return
+     */
+    public E getFirst() {
+        return get(0);
+    }
+
+    /**
+     * 获取链表最后一个元素
+     *
+     * @return
+     */
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    /**
+     * 修改链表中为index位置的元素为e
+     *
+     * @param index
+     * @param e
+     */
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed,Illeagal index");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    /**
+     * 查找链表中是否有元素e
+     *
+     * @param e
+     * @return
+     */
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+//        Node cur = dummyHead.next;
+//        while (cur != null) {
+//            res.append(cur + "->");
+//            cur = cur.next;
+//        }
+        for(Node cur = dummyHead.next;cur!=null;cur=cur.next){
+            res.append(cur + "->");
+        }
+        res.append("NULL");
+        return res.toString();
     }
 }
