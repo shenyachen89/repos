77af35117e71e5554a5249550d6ae39b07c41177
shenyachen <shenyachen@fuljoy.com>
2019-06-14 15:42:14 +0800

    leetcode 链表题

---
+++ data_structures/src/com/knife/linkedlist/Solution.java
@@ -0,0 +1,46 @@
+package com.knife.linkedlist;
+
+public class Solution {
+    /**
+     * Remove all elements from a linked list of integers that have value val.
+     * <p>
+     * Example:
+     * <p>
+     * Input:  1->2->6->3->4->5->6, val = 6
+     * Output: 1->2->3->4->5
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
