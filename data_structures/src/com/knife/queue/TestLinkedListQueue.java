b68d2479be4a7259b6fccee5d3229d5139ce355e
shenyachen <shenyachen@fuljoy.com>
2019-06-14 10:31:34 +0800

    使用链表实现队列

---
+++ data_structures/src/com/knife/queue/TestLinkedListQueue.java
@@ -0,0 +1,17 @@
+package com.knife.queue;
+
+public class TestLinkedListQueue {
+
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}
